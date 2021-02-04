76c2e34f6b93be6a7566690637adba8cf2cb34c0
IrhamFadzillah <irham8381@gmail.com>
2021-02-04 23:07:17 +0000

    pylint: auto fixes
    Signed-off-by: IrhamFadzillah <irham8381@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -335,14 +335,13 @@ ISAFK = False
 AFKREASON = None
 ZALG_LIST = {}
 
-
- @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
-    async def on_plug_in_callback_query_handler(event):
-        if event.query.user_id == bot.uid:
-            await event.edit("menu closed")
-        else:
-            reply_pop_up_alert = "Please get your own Yama, and don't use mine! Join @NightCoreUserbot help "
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+  @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
+      async def on_plug_in_callback_query_handler(event):
+           if event.query.user_id == bot.uid:
+                await event.edit("menu closed")
+            else:
+                reply_pop_up_alert = "Please get your own Yama, and don't use mine! Join @NightCoreUserbot help "
+                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
 def paginate_help(page_number, loaded_modules, prefix):
