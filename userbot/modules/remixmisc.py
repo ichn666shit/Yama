d546e76d71f2a5699728b4bf5fcf1a48fb7fc29e
Irham <74205184+IrhamFadzillah@users.noreply.github.com>
2021-02-05 00:26:38 +0700

    Update remixmisc.py

--- userbot/modules/remixmisc.py
+++ userbot/modules/remixmisc.py
@@ -487,65 +487,6 @@ async def _(event):
             await event.client.delete_message(chat, event.chat_id, response.message)
 
 
-@register(outgoing=True, pattern="^.clone(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    reply_message = await event.get_reply_message()
-    replied_user, error_i_a = await get_full_user(event)
-    if replied_user is None:
-        await event.edit(str(error_i_a))
-        return False
-    user_id = replied_user.user.id
-    profile_pic = await event.client.download_profile_photo(user_id, TEMP_DOWNLOAD_DIRECTORY)
-    # some people have weird HTML in their names
-    first_name = html.escape(replied_user.user.first_name)
-    # https://stackoverflow.com/a/5072031/4723940
-    # some Deleted Accounts do not have first_name
-    if first_name is not None:
-        # some weird people (like me) have more than 4096 characters in their
-        # names
-        first_name = first_name.replace("\u2060", "")
-    last_name = replied_user.user.last_name
-    # last_name is not Manadatory in @Telegram
-    if last_name is not None:
-        last_name = html.escape(last_name)
-        last_name = last_name.replace("\u2060", "")
-    if last_name is None:
-        last_name = "⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
-    # inspired by https://telegram.dog/afsaI181
-    user_bio = replied_user.about
-    if user_bio is not None:
-        user_bio = html.escape(replied_user.about)
-    await bot(functions.account.UpdateProfileRequest(
-        first_name=first_name
-    ))
-    await bot(functions.account.UpdateProfileRequest(
-        last_name=last_name
-    ))
-    await bot(functions.account.UpdateProfileRequest(
-        about=user_bio
-    ))
-    pfile = await bot.upload_file(profile_pic)  # pylint:disable=E060
-    await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
-        pfile
-    ))
-    #message_id_to_reply = event.message.reply_to_msg_id
-    # if not message_id_to_reply:
-    #    message_id_to_reply = event.message.id
-    # await bot.send_message(
-    #  event.chat_id,
-    #  "Hey ? Whats Up !",
-    #  reply_to=message_id_to_reply,
-    #  )
-    await event.delete()
-    await bot.send_message(
-        event.chat_id,
-        "**User Berhasil di Clone!!.**",
-        reply_to=reply_message
-    )
-
-
 async def get_full_user(event):
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
@@ -1042,8 +983,6 @@ CMD_HELP.update({
 \nUsage:replay .grab or .grab <count> to grab profile picture.\
 \n\n`.rnupload` filename.extenstion\
 \nUsage:reply to a sticker and type .rnupload xyz.jpg\
-\n\n`.clone` @username and '.rclone' for reverting\
-\nUsage: clone you whole freking account except username so stay safe\
 \n\n`.res`\
 \nUsage: type account,channel,group or bot username and reply with .res and check restriction\
 \n\n`.watch` <movie/tv> show\
