6a76fb828252a59ab211b3d7aae43dc59d81a4be
IrhamFadzillah <irham8381@gmail.com>
2021-02-04 16:00:15 +0000

    pylint: auto fixes
    Signed-off-by: IrhamFadzillah <irham8381@gmail.com>

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -25,12 +25,12 @@ async def help(event):
             await event.delete()
     else:
         await event.edit(f"** **\
-            \n"\   Help for [Yama Bot]\
-            \n"\
-            \n"\   Untuk melihat lengkap Command\
-            \n"\   Contoh: .help <nama module>\
-            \n"\   Modules Aktif: {len(modules)}\
-           \n")
+            \n"\   Help for [Yama Bot]
+                         \n"\
+            \n"\   Untuk melihat lengkap Command
+                         \n"\\   Contoh: .help <nama module>\
+            \n"\   Modules Aktif: {len(modules)}
+                         \n")
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
